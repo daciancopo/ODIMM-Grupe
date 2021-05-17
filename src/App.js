05fd9dc4955d9a0222af96ff60abae94ee5584e1
CopoceanDacian <dacian.copocean@ferendia.com>
2021-05-17 12:58:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import React from "react"
-// import search from "./images/Search.png";
-// import arrowUp from "./images/Arrow-up.png";
-// import arrowDown from "./images/Arrow-down.png";
-// import letter from "./images/Letter.png";
-// import dots from "./images/Dots.png";
-// import maria from "./images/Maria.png";
 import './App.css';
 import PageCopy from "./components/PageCopy/pageCopy";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     show: false,
-  //   };
 
-  // }
-
-  // show = () => {
-  //   this.setState({
-  //     show: !this.state.show
-  //   })
-  // }
   render() {
     return (
       <div className="container mt-3 ">
